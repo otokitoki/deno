b4713db53de7ecf327b1354cb5909a0729d84258
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-04-04 21:10:06 +0900

    Update deno-first.ts

--- deno-first.ts
+++ deno-first.ts
@@ -1,8 +1,8 @@
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: FetchEvent) => {
   // Get the value of the header named `x-forwarded-for`.
   // The value can either be a IPv4 or IPv6 address.
-  const ip = event.request.headers.get("x-forwarded-for");
-  const response = new Response(`Your IP address is <b>${ip}</b>`, {
+  const ip: number | string = event.request.headers.get("x-forwarded-for");
+  const response: Response = new Response(`Your IP address is <b>${ip}</b>`, {
     headers: { "content-type": "text/html" },
   });
   event.respondWith(response);
