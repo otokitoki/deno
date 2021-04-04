ae0f574da5739d3a2ce053cfb24696d3a5ea24db
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-04-04 20:15:50 +0900

    Update deno-first.ts

--- deno-first.ts
+++ deno-first.ts
@@ -1,6 +1,9 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8000 });
-console.log("hello world");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+addEventListener("fetch", (event) => {
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+  const response = new Response(`Your IP address is <b>${ip}</b>`, {
+    headers: { "content-type": "text/html" },
+  });
+  event.respondWith(response);
+});
